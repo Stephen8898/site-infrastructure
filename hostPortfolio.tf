dbeb21adca22be04e8f01b53ec112e32a290a279
Stephen Sarfo <stephen.sarfo1998@outlook.com>
2024-07-08 14:55:15 -0400

    Fixed viewer certificate

--- hostPortfolio.tf
+++ hostPortfolio.tf
@@ -102,15 +102,19 @@ resource "aws_cloudfront_distribution" "distribution" {
       http_port                = 80
       https_port               = 443
       origin_keepalive_timeout = 5
-      origin_protocol_policy   = "http-only"
+      origin_protocol_policy   = "match-viewer"
       origin_read_timeout      = 30
-      origin_ssl_protocols     = ["TLSv1.2"]
+      origin_ssl_protocols     = ["TLSv1", "TLSv1.1", "TLSv1.2"]
     }
   }
 
   aliases = [var.domain_name]
 
-  viewer_certificate { acm_certificate_arn = var.certificate_arn }
+  viewer_certificate {
+    acm_certificate_arn = var.certificate_arn
+    ssl_support_method  = "sni-only"
+
+  }
 
   restrictions {
     geo_restriction {
